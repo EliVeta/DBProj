b03d0b1933d6cdb588230f327ae69994d20a7b44
EliVeta <105371929+EliVeta@users.noreply.github.com>
2023-07-01 22:26:27 +0700

    убраны лишние строки

--- app/src/main/java/com/example/dbproj/MainActivity.java
+++ app/src/main/java/com/example/dbproj/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.dbproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+//import android.database.Cursor;
+//import android.database.sqlite.SQLiteDatabase;
 //import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.provider.BaseColumns;
+//import android.provider.BaseColumns;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,63 +17,36 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class MainActivity extends AppCompatActivity {
-    //SQLiteOpenHelper sqLiteOpenHelper;
     DBHelper dbHelper;
+    DBManager dbManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper = new DBHelper(this);//???
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        dbHelper = new DBHelper(this);
+        dbManager = new DBManager(this);
 
-        String[] projection = {
-                BaseColumns._ID,
-                TPlace.Place.COLUMN_NAME_NAMEPLACE
-        };
 
-        // How you want the results sorted in the resulting Cursor
-        String sortOrder =
-                TPlace.Place.COLUMN_NAME_NAMEPLACE + " DESC";
 
-        Cursor cursor = db.query(
-                TPlace.Place.TABLE_NAME,   // The table to query
-                projection,             // The array of columns to return (pass null to get all)
-                null,              // The columns for the WHERE clause
-                null,          // The values for the WHERE clause
-                null,                   // don't group the rows
-                null,                   // don't filter by row groups
-                sortOrder               // The sort order
-        );
+    }
 
-        List itemIds = new ArrayList<>();
-        while(cursor.moveToNext()) {
-            long itemId = cursor.getLong(
-                    cursor.getColumnIndexOrThrow(TPlace.Place._ID));
-            itemIds.add(itemId);
-        }
-        cursor.close();
+    @Override
+    protected void onResume(){
+        super.onResume();
+        dbManager.openDB();
 
-        ArrayAdapter<String> namePlaceAdapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_spinner_item, itemIds
+        ArrayAdapter<String> namePlaceAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, dbManager.getFromDBNamePlace()
         );
         namePlaceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spNamePlace = (Spinner) findViewById(R.id.spinnerPlace);
         spNamePlace.setAdapter(namePlaceAdapter);
 
-        spNamePlace.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getBaseContext(), adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+        spNamePlace.setOnItemSelectedListener(onItemSelectedListener());
     }
-
     @Override
     protected void onDestroy() {
         dbHelper.close();
@@ -86,6 +59,19 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-   /* public void ClickChoisePlace(View view) {
-    }*/
+
+    AdapterView.OnItemSelectedListener onItemSelectedListener(){
+        return new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getBaseContext(), adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        };
+    }
 }

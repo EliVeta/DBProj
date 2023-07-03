28052615eb05feeedc814ad469a530dce44c779d
EliVeta <105371929+EliVeta@users.noreply.github.com>
2023-07-03 16:17:47 +0700

    БД работает (подгружаются данные, можно добавить новые), карта открывается, но пока без меток

--- app/src/main/java/com/example/dbproj/MainActivity.java
+++ app/src/main/java/com/example/dbproj/MainActivity.java
@@ -14,6 +14,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,15 +28,43 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     DBHelper dbHelper;
     DBManager dbManager;
+    MapView mapview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        MapKitFactory.setApiKey("b36689fb-9a9f-499b-a707-c2a23f7acd92");
+        MapKitFactory.initialize(this);
+
         setContentView(R.layout.activity_main);
         dbHelper = new DBHelper(this);
         dbManager = new DBManager(this);
 
+        mapview = (MapView)findViewById(R.id.mapview);
+        mapview.getMap().move(
+                new CameraPosition(
+                        new Point(55.751574,37.573856), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
 
 
+    }
+
+    @Override
+    protected void onStop() {
+
+        mapview.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+
+        mapview.onStart();
+        MapKitFactory.getInstance().onStart();
+        super.onStart();
     }
 
     @Override
@@ -73,5 +107,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+    }
+
+    public void ClickGoToMap(View view) {
+        Intent intent=new Intent(MainActivity.this,MapActivity.class);
+        startActivity(intent);
     }
 }
